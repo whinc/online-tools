3807100ae1fbeb7f299b386100e0ad1475ee23fd
whincwu <whincwu@163.com>
2019-12-27 14:47:03 +0800

    feat: 测试CI

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Online tools
+          Online tools - test CI
         </p>
       </header>
     </div>
