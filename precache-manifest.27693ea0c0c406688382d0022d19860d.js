cfd4424b5eabcd8a5eb5f49f2f7aa146e439d75e
whincwu <whincwu@163.com>
2019-12-27 11:21:23 +0800

    Updates

---
+++ precache-manifest.27693ea0c0c406688382d0022d19860d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3b804182dd4664e98f1ab3f3356558",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df80e5aa50784b6124ce",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1ffcaf16c52feb744101",
+    "url": "/static/js/2.19a944aa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.19a944aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df80e5aa50784b6124ce",
+    "url": "/static/js/main.d5900518.chunk.js"
+  },
+  {
+    "revision": "c0bf25e875db0d1bb9db",
+    "url": "/static/js/runtime-main.42ec590d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
