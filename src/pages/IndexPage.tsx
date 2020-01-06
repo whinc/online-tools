c79486cb682782efa23e31edbc3d86d36dbd61d7
whincwu <whincwu@163.com>
2020-01-07 00:04:26 +0800

    feat: 首页增加正则表达式封面

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,27 +1,37 @@
 
 import React from "react";
-import {Box, GridList, GridListTile, ListSubheader, GridListTileBar} from '@material-ui/core'
+import {Box, GridList, GridListTile, ListSubheader, GridListTileBar, IconButton} from '@material-ui/core'
+import {makeStyles} from '@material-ui/core/styles'
 import PageLayout from 'layout/PageLayout'
 import {useHistory} from 'react-router-dom'
+import regexp from 'assets/regexp.svg'
+import { Launch } from "@material-ui/icons";
+
+const useStyles = makeStyles({
+  clickable: {
+    '&:hover': {
+      cursor: 'pointer'
+    }
+  }
+})
 
 const IndexPage: React.FC = () => {
+  const styles = useStyles()
   const history = useHistory()
   return (
     <PageLayout>
       <GridList cellHeight={200} cols={2}>
-        <GridListTile style={{height: 'auto'}} cols={2}>
-          <ListSubheader component="div">Online Tools</ListSubheader>
-        </GridListTile>
-        <GridListTile onClick={() => history.push('/regexp')}>
-          <Box bgcolor='primary.main' height={'100%'}/>
+        <GridListTile className={styles.clickable} onClick={() => history.push('/regexp')}>
+          <img src={regexp} alt='正则表达式' />
           <GridListTileBar
             title='正则表达式'
+            actionIcon={<IconButton><Launch htmlColor='white'/></IconButton>}
           />
         </GridListTile>
-        <GridListTile>
+        <GridListTile className={styles.clickable}>
           <Box bgcolor='primary.main' height={'100%'}/>
           <GridListTileBar
-            title='glob 模式'
+            title='开发中...'
           />
         </GridListTile>
       </GridList>
